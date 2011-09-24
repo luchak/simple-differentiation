24db06d1db7ba4daa3b9213f0df6fbebac55f748
Matt Stanton <matt@mattstanton.com>
2011-09-24 11:03:46 -0400

    Finish vector arithmetic tests.

--- differentiation_test.cc
+++ differentiation_test.cc
@@ -1,13 +1,18 @@
 #include "vector.h"
 #include "differentiation.h"
 
+#include <vector>
+
 #include <gtest/gtest.h>
 
 namespace {
 
 TEST(VectorTest, VectorCreation) {
-  simple_differentiation::Vector<double> vec0(0);
+  simple_differentiation::Vector<double> vec0;
   EXPECT_EQ(0, vec0.size());
+
+  simple_differentiation::Vector<double> vec0a(0);
+  EXPECT_EQ(0, vec0a.size());
 
   simple_differentiation::Vector<double> vec1(1);
   EXPECT_EQ(1, vec1.size());
@@ -21,6 +26,185 @@ TEST(VectorTest, VectorCreation) {
   EXPECT_EQ(2, vec2.size());
   EXPECT_EQ(7.0, vec2[0]);
   EXPECT_EQ(7.0, vec2[1]);
+
+  simple_differentiation::Vector<int> vec1i(1, -1);
+  EXPECT_EQ(1, vec1i.size());
+  EXPECT_EQ(-1, vec1i[0]);
+
+  std::vector<double> src;
+  src.push_back(0.1);
+  src.push_back(0.2);
+  simple_differentiation::Vector<double> dst(src.begin(), src.end());
+  EXPECT_EQ(src.size(), dst.size());
+  EXPECT_EQ(src[0], dst[0]);
+  EXPECT_EQ(src[1], dst[1]);
+}
+
+TEST(VectorTest, VectorNegation) {
+  simple_differentiation::Vector<double> vec0(0);
+  EXPECT_EQ(0, (-vec0).size());
+
+  simple_differentiation::Vector<double> vec1(1, 4.0);
+  EXPECT_EQ(1, (-vec1).size());
+  EXPECT_EQ(-4.0, (-vec1)[0]);
+
+  simple_differentiation::Vector<double> vec2(1, -4.0);
+  vec2.push_back(0.5);
+  EXPECT_EQ(2, (-vec2).size());
+  EXPECT_EQ(4.0, (-vec2)[0]);
+  EXPECT_EQ(-0.5, (-vec1)[1]);
+}
+
+TEST(VectorTest, VectorAddAssignment) {
+  simple_differentiation::Vector<double> vec_a;
+  vec_a.push_back(1.0);
+  vec_a.push_back(2.0);
+  simple_differentiation::Vector<double> vec_b;
+  vec_b.push_back(3.0);
+  vec_b.push_back(-1.0);
+
+  simple_differentiation::Vector<double> vec_a_copy = (vec_a += vec_b);
+  EXPECT_EQ(2, vec_a.size());
+  EXPECT_EQ(4.0, vec_a[0]);
+  EXPECT_EQ(1.0, vec_a[1]);
+
+  EXPECT_EQ(vec_a.size(), vec_a_copy.size());
+  EXPECT_EQ(vec_a[0], vec_a_copy[0]);
+  EXPECT_EQ(vec_a[1], vec_a_copy[1]);
+}
+
+TEST(VectorTest, VectorAddition) {
+  simple_differentiation::Vector<double> vec_a;
+  vec_a.push_back(1.0);
+  vec_a.push_back(2.0);
+  simple_differentiation::Vector<double> vec_b;
+  vec_b.push_back(3.0);
+  vec_b.push_back(-1.0);
+
+  simple_differentiation::Vector<double> vec_c = vec_a + vec_b;
+  EXPECT_EQ(2, vec_c.size());
+  EXPECT_EQ(4.0, vec_c[0]);
+  EXPECT_EQ(1.0, vec_c[1]);
+}
+
+TEST(VectorTest, VectorSubtractAssignment) {
+  simple_differentiation::Vector<double> vec_a;
+  vec_a.push_back(1.0);
+  vec_a.push_back(2.0);
+  simple_differentiation::Vector<double> vec_b;
+  vec_b.push_back(3.0);
+  vec_b.push_back(-1.0);
+
+  simple_differentiation::Vector<double> vec_a_copy = (vec_a -= vec_b);
+  EXPECT_EQ(2, vec_a.size());
+  EXPECT_EQ(-2.0, vec_a[0]);
+  EXPECT_EQ(3.0, vec_a[1]);
+
+  EXPECT_EQ(vec_a.size(), vec_a_copy.size());
+  EXPECT_EQ(vec_a[0], vec_a_copy[0]);
+  EXPECT_EQ(vec_a[1], vec_a_copy[1]);
+}
+
+TEST(VectorTest, VectorSubtraction) {
+  simple_differentiation::Vector<double> vec_a;
+  vec_a.push_back(1.0);
+  vec_a.push_back(2.0);
+  simple_differentiation::Vector<double> vec_b;
+  vec_b.push_back(3.0);
+  vec_b.push_back(-1.0);
+
+  simple_differentiation::Vector<double> vec_c = vec_a - vec_b;
+  EXPECT_EQ(2, vec_c.size());
+  EXPECT_EQ(-2.0, vec_c[0]);
+  EXPECT_EQ(3.0, vec_c[1]);
+}
+
+TEST(VectorTest, VectorMultiplyAssignment) {
+  simple_differentiation::Vector<double> vec;
+  vec.push_back(1.0);
+  vec.push_back(2.0);
+
+  simple_differentiation::Vector<double> vec_copy = (vec *= 3.0);
+  EXPECT_EQ(2, vec.size());
+  EXPECT_EQ(3.0, vec[0]);
+  EXPECT_EQ(6.0, vec[1]);
+
+  EXPECT_EQ(vec.size(), vec_copy.size());
+  EXPECT_EQ(vec[0], vec_copy[0]);
+  EXPECT_EQ(vec[1], vec_copy[1]);
+
+  vec.clear();
+  vec.push_back(1.0);
+  vec.push_back(2.0);
+  vec *= 2;
+  EXPECT_EQ(2, vec.size());
+  EXPECT_EQ(2.0, vec[0]);
+  EXPECT_EQ(4.0, vec[1]);
+}
+
+TEST(VectorTest, VectorMultiplication) {
+  simple_differentiation::Vector<double> vec;
+  vec.push_back(1.0);
+  vec.push_back(2.0);
+
+  simple_differentiation::Vector<double> vec_mult = vec * 3.0;
+  EXPECT_EQ(2, vec_mult.size());
+  EXPECT_EQ(3.0, vec_mult[0]);
+  EXPECT_EQ(6.0, vec_mult[1]);
+
+  vec_mult = vec * 2;
+  EXPECT_EQ(2, vec_mult.size());
+  EXPECT_EQ(2.0, vec_mult[0]);
+  EXPECT_EQ(4.0, vec_mult[1]);
+
+  vec_mult = 4.0 * vec;
+  EXPECT_EQ(2, vec_mult.size());
+  EXPECT_EQ(4.0, vec_mult[0]);
+  EXPECT_EQ(8.0, vec_mult[1]);
+
+  vec_mult = 5 * vec;
+  EXPECT_EQ(2, vec_mult.size());
+  EXPECT_EQ(5.0, vec_mult[0]);
+  EXPECT_EQ(10.0, vec_mult[1]);
+}
+
+TEST(VectorTest, VectorDivideAssignment) {
+  simple_differentiation::Vector<double> vec;
+  vec.push_back(60.0);
+  vec.push_back(120.0);
+
+  simple_differentiation::Vector<double> vec_copy = (vec /= 3.0);
+  EXPECT_EQ(2, vec.size());
+  EXPECT_EQ(20.0, vec[0]);
+  EXPECT_EQ(40.0, vec[1]);
+
+  EXPECT_EQ(vec.size(), vec_copy.size());
+  EXPECT_EQ(vec[0], vec_copy[0]);
+  EXPECT_EQ(vec[1], vec_copy[1]);
+
+  vec.clear();
+  vec.push_back(60.0);
+  vec.push_back(120.0);
+  vec /= 2;
+  EXPECT_EQ(2, vec.size());
+  EXPECT_EQ(30.0, vec[0]);
+  EXPECT_EQ(60.0, vec[1]);
+}
+
+TEST(VectorTest, VectorDivision) {
+  simple_differentiation::Vector<double> vec;
+  vec.push_back(60.0);
+  vec.push_back(120.0);
+
+  simple_differentiation::Vector<double> vec_div = vec / 3.0;
+  EXPECT_EQ(2, vec_div.size());
+  EXPECT_EQ(20.0, vec_div[0]);
+  EXPECT_EQ(40.0, vec_div[1]);
+
+  vec_div = vec / 2;
+  EXPECT_EQ(2, vec_div.size());
+  EXPECT_EQ(30.0, vec_div[0]);
+  EXPECT_EQ(60.0, vec_div[1]);
 }
 
 }  // namespace
